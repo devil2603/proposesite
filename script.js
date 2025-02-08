79f2dc43be310c561315065be1cef458787eade7
vasant J <86471065+devil2603@users.noreply.github.com>
2025-02-08 17:09:45 +0530

    all files

---
+++ script.js
@@ -0,0 +1,30 @@
+// const form = document.getElementById('login-form');
+// const codeInput = document.getElementById('code');
+// const submitBtn = document.getElementById('submit-btn');
+// const resultDiv = document.getElementById('result');
+
+// const secretCode = 'Durga'; // Replace with your secret code
+// const nextPageUrl = 'main.html'; // Replace with the URL of the new page
+
+// form.addEventListener('submit', (e) => {
+// 	e.preventDefault();
+// 	const userInput = codeInput.value.trim();
+// 	if (userInput === secretCode) {
+// 		resultDiv.innerHTML = 'Baby';
+// 		window.location.href = nextPageUrl; // Redirect to new page
+// 	} else {
+// 		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri Sona';
+// 	}
+// });
+
+const form = document.getElementById('login-form');
+const codeInput = document.getElementById('code');
+const submitBtn = document.getElementById('submit-btn');
+const resultDiv = document.getElementById('result');
+
+const nextPageUrl = 'main.html'; // Replace with the URL of the new page
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	window.location.href = nextPageUrl; // Redirect to new page
+});
